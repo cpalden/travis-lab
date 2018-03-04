fc35977ff016bfda09d4142d4d3c7d10723ead9c
cpalden <cpalden@scs.ryerson.ca>
2018-03-04 00:18:49 -0500

    modidied test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -21,7 +21,7 @@ class TestCalc(unittest.TestCase):
     def test_divide(self):
         self.assertEqual(calc.divide(10, 5), 2)
         self.assertEqual(calc.divide(1, 1), 1)
-        self.assertEqual(calc.divide(5, 2), 2.5)
+        self.assertEqual(calc.divide(4, 2), 2)
 
         with self.assertRaises(ValueError):
             calc.divide(10, 0)
